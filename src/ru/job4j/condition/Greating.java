23671bddabe02b68d5f6eaac617a8a16ac1b7345
afoni4 <afoni4@gmail.com>
2021-03-04 19:05:38 +0300

    9. Площадь треугольника. [#484590]

---
+++ src/ru/job4j/condition/Greating.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greating {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String result = idea + "But I am a newbie.";
+        int year = 2021;
+        String result2 = result + year;
+        System.out.println(result2);
+
+    }
+}
