3233aff9d05d7a85343eb41ced2b14ab5cbf7100
afoni4 <afoni4@gmail.com>
2021-03-06 12:25:38 +0300

    1.1. Входные данные в тесте [#484622]

--- test/ru/job4j/caculate/X2Test.java
+++ test/ru/job4j/caculate/X2Test.java
@@ -7,21 +7,10 @@ import org.junit.Assert;
 public class X2Test {
 
     @Test
-    public void whenA10B0C0X1Then40() {
+    public void whenA10B0C0X1Then10() {
         int a = 10;
         int b = 0;
         int c = 0;
-        int x = 1;
-        int expected = 40;
-        int rsl = X2.calc(a, b, c, x);
-        Assert.assertEquals(expected, rsl);
-    }
-
-    @Test
-    public void whenA1B1C1X1Then10() {
-        int a = 1;
-        int b = 1;
-        int c = 1;
         int x = 1;
         int expected = 10;
         int rsl = X2.calc(a, b, c, x);
@@ -29,34 +18,45 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then11() {
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = 11;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenA1B1C1X1Then12() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 12;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenA1B1C1X1Then13() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
-        int expected = 13;
+        int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
