218f6b509c3faba152ba34443669b0bdc1958817
afoni4 <afoni4@gmail.com>
2021-03-10 09:33:32 +0300

    5.5. Простое число [#484644]

--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.*;
 public class CheckPrimeNumberTest {
     @Test
     public void when5() {
-        boolean rsl = CheckPrimeNumber.check(3);
+        boolean rsl = CheckPrimeNumber.check(5);
         assertThat(rsl, is(true));
     }
 
     @Test
     public void when4() {
-        boolean rsl = CheckPrimeNumber.check(2);
+        boolean rsl = CheckPrimeNumber.check(4);
         assertThat(rsl, is(false));
     }
 
