eabf94791776d9b83bf36bb38c9679c0acb8b163
afoni4 <afoni4@gmail.com>
2021-03-11 16:09:48 +0300

    5.7. Ипотека [#484643]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double credit = amount + amount * percent / 100;
-        while (credit > 0) {
-            double debt = credit - salary;
-            credit = debt * percent / 100 + debt;
+        while (amount > 0) {
+            amount = (int) (amount + amount * percent / 100);
+            amount -= salary;
             year++;
         }
         return year;
