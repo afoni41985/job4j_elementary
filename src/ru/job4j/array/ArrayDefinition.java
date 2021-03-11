957554a0e7acb613f9308965dcaf60cf469aae3f
afoni4 <afoni4@gmail.com>
2021-03-11 21:14:52 +0300

    6.0.2. Заполнение массива. [#484565]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,6 +2,12 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Oleg Petrov";
+        names[3] = "Ivan Cidorov";
+
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
