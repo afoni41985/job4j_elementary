ceb7dc2bbfcd2891850158a92c4f67ee04b67476
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-18 17:10:45 +0300

    2. Вывести на консоль числа от N до M

---
+++ src/ru/job4j/loop/PrintNToM.java
@@ -0,0 +1,10 @@
+package ru.job4j.loop;
+
+/*Выведите на консоль числа от N до M. M не входит. В этом упражнении используйте цикл for c index.*/
+public class PrintNToM {
+    public static void out(int n, int m) {
+        for (int index = n; index < m; index++) {
+            System.out.println(index);
+        }
+    }
+}
