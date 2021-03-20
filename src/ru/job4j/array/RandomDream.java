9c24fb8f1ca67add984047a81075b32dc2a36399
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-20 19:06:01 +0300

    44. Поле чудес

---
+++ src/ru/job4j/array/RandomDream.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/*Дан массив призов и положительное число, которое сгенерировано случайно. Ваша задача получить приз
+Например,
+{"автомобиль", "квартира", "билет"} и число = 1 => "автомобиль"
+{"автомобиль", "квартира", "билет"} и число = 2 => "квартира"
+{"автомобиль", "квартира", "билет"} и число = 4 => "автомобиль"
+{"автомобиль", "квартира", "билет"} и число = 99 => "билет"
+ */
+public class RandomDream {
+    public static String random(String[] prizes, int num) {
+        String prize = null;
+        if (num < 4) {
+            num += 4;
+        }
+        int xxx = num % 4;
+        switch (xxx) {
+            case 1 -> prize = prizes[0];
+                case 2 -> prize = prizes[1];
+                case 3 -> prize = prizes[2];
+                case 0 -> prize = prizes[3];
+        }
+        return prize;
+    }
+}
+
+
+
