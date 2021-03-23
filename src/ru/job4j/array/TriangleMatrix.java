dddda69e4212772d8f721aec219529d680887595
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-23 14:56:47 +0300

    16. Заполнить треугольную матрицу

--- src/ru/job4j/array/TriangleMatrix.java
+++ src/ru/job4j/array/TriangleMatrix.java
@@ -16,11 +16,11 @@ import java.util.Arrays;
 public class TriangleMatrix {
     public static int[][] rows(int count) {
         int[][] triangle = new int[count][];
-        int elem = 1;
+        int counter = 1;
         for (int i = 0; i < triangle.length; i++) {
             triangle[i] = new int[i + 1];
-            for (int j = 0; j < triangle[i].length; j++) {
-                triangle[i][j] = elem++;
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = counter++;
             }
         }
         return triangle;
