528137e24ec169dec459b8f79fd194e1a88fd58c
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-23 15:22:00 +0300

    1. Экзамен. Базовый синтаксис. [#484628]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,20 +1,23 @@
 package ru.job4j.array;
 
-/* Даны два отсортированных по возрастанию массива.
- Как без сортировки их объединить в третий массив?
- */
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int leftIndex = 0;
-        int rightIndex = 0;
-        while (leftIndex + rightIndex < rsl.length) {
-            if (leftIndex < left.length && left[leftIndex] < right[rightIndex]) {
-                rsl[leftIndex + rightIndex] = left[leftIndex++];
-
+        int lIndex = 0;
+        int rIndex = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            if (lIndex >= left.length) {
+                rsl[i] = right[rIndex];
+                rIndex += 1;
+            } else if (rIndex >= left.length) {
+                rsl[i] = left[lIndex];
+                lIndex += 1;
+            } else if (left[lIndex] < right[rIndex]) {
+                rsl[i] = left[lIndex];
+                lIndex += 1;
             } else {
-                rsl[leftIndex + rightIndex] = right[rightIndex++];
-
+                rsl[i] = right[rIndex];
+                rIndex += 1;
             }
         }
         return rsl;
